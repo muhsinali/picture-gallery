e0b48c1e86a783b78bd21e27856997aaf07c5cd7
Muhsin Ali <muhsinali@users.noreply.github.com>
2017-02-05 22:30:49 +0000

    Add tests for the Application controller

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,19 +1,45 @@
+import controllers.PlaceDAO;
 import org.junit.Test;
+import play.mvc.Result;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.*;
 
 
-/**
-*
-* Simple (JUnit) tests that can call all parts of a play app.
-* If you are interested in mocking a whole application, see the wiki for more details.
-*
-*/
 public class ApplicationTest {
+    @Test
+    public void showList(){
+        running(fakeApplication(), () -> {
+            Result result = callAction(
+                controllers.routes.ref.Application.showList(),
+                fakeRequest()
+            );
+            assertThat(status(result)).isEqualTo(OK);
+        });
+    }
 
     @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
+    public void deleteAPlace(){
+        running(fakeApplication(), () -> {
+            int numPlaces = PlaceDAO.getNumberOfPlaces();
+            assertThat(numPlaces).isGreaterThan(0);
+            Result result = callAction(
+                controllers.routes.ref.Application.deletePlace(1),
+                fakeRequest()
+            );
+            assertThat(status(result)).isEqualTo(OK);
+            assertThat(PlaceDAO.getNumberOfPlaces()).isEqualTo(numPlaces - 1);
+        });
+    }
+
+    @Test
+    public void getPictureOfPlace(){
+        running(fakeApplication(), () -> {
+            Result result = callAction(
+                controllers.routes.ref.Application.getPictureOfPlace(1),
+                fakeRequest()
+            );
+            assertThat(status(result)).isEqualTo(OK);
+        });
     }
 }
